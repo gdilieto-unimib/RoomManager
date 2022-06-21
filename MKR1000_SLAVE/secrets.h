429556b5407fd95537c8d32bbe379b0281585799
Tiberio Falsiroli <t.falsiroli@campus.unimib.it>
2022-06-21 10:56:59 +0200

    fix credentials

--- MKR1000_SLAVE/secrets.h
+++ MKR1000_SLAVE/secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials and connection details
 
 // WiFi configuration
-#define SECRET_SSID "IoTLabThingsU14"                  // SSID
-#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Lessons!"              // WiFi password
+#define SECRET_SSID "iPhone (2)"                  // SSID
+#define SECRET_PASS "labiot11"              // WiFi password
 
 // MQTT access
 #define MQTT_BROKERIP "149.132.178.180"         // IP address of the machine running the MQTT broker
